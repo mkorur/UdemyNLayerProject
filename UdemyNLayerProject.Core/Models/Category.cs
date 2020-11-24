1a33fb177e412a3114f7d83848569eb8eb5b9b06
Mustafa <mkorur@outlook.com>
2020-11-24 22:35:45 +0300

    Add project files.

---
+++ UdemyNLayerProject.Core/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace UdemyNLayerProject.Core.Models
+{
+    class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ UdemyNLayerProject.Core/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyNLayerProject.Core.Repositories
+{
+    interface IRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int Id);
+
+        Task<List<TEntity>> GetAllAsync();
+        Task<List<TEntity>> Find();
+
+    }
+}
